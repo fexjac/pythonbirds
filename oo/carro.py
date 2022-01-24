d7fc978d6ce3dc70c0966630391c61f0afef8799
fexjac <fexjac@gmail.com>
2022-01-23 22:16:34 -0400

    Implantação de estudo de testes

--- oo/carro.py
+++ oo/carro.py
@@ -91,8 +91,31 @@ Exemplo:
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
     'Oeste'
-
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+       return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 
 NORTE = 'Norte'
 SUL = 'Sul'
@@ -130,30 +153,6 @@ class Motor:
         self.velocidade = max(0, self.velocidade)
 
 
-class Carro:
-    def __init__(self, direcao, motor):
-        self.direcao = direcao
-        self.motor = motor
-
-    def calcular_velocidade(self):
-        velocidade = motor.velocidade
-        return velocidade
-
-    def calcular_direcao(self):
-        direcao = self.direcao.valor()
-        return direcao
-
-    def acelerar(self):
-        self.motor.acelerar()
-
-    def frear(self):
-        self.motor.acelerar()
-
-    def girar_a_direita(self):
-        self.direcao.girar_a_direita()
-
-    def girar_a_esquerda(self):
-        self.direcao.girar_a_esquerda()
 
 
 
